d57f5123af10672a2dde9ec2709ad44c9f6ac9d6
ouzhou <ouzz413@outlook.com>
2026-04-13 18:32:41 +0800

    fix

--- packages/core-v2/src/render/locate.ts
+++ packages/core-v2/src/render/locate.ts
@@ -1,6 +1,9 @@
 import canvaskitWasmUrl from "canvaskit-wasm/bin/canvaskit.wasm?url";
 
 export function canvasKitLocateFile(file: string): string {
+  if (file.endsWith(".wasm")) {
+    return canvaskitWasmUrl;
+  }
   const slash = canvaskitWasmUrl.lastIndexOf("/");
   const base = slash >= 0 ? canvaskitWasmUrl.slice(0, slash + 1) : canvaskitWasmUrl;
   return `${base}${file}`;
