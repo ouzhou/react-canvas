085f0ea66f50d3655753603d1ef2d53331887f72
ouzhou <ouzz413@outlook.com>
2026-04-14 16:08:03 +0800

    router

--- apps/v3/src/router.tsx
+++ apps/v3/src/router.tsx
@@ -1,9 +1,9 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { App } from "./App.tsx";
 import { HomePage } from "./routes/home-page.tsx";
 import { JuejinPage } from "./routes/juejin-page.tsx";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <App />,
